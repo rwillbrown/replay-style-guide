72c7c806d4759c647cdb9fce9c49af72671387e1
Jordan Carmon <jordan@matchstickstudio.co>
2017-03-06 03:01:10 -0600

    Showing the grid system and started on the site's header.

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,4 +1,4 @@
 // this file just rolls up all the components so the app can use them.
 
-export * from './widget/widget'; // Relative to /src/app/components, this points to the .ts file in each component directory.
+export * from './site-header/site-header'; // Relative to /src/app/components, this points to the .ts file in each component directory.
 //export * from './otherWidget/otherWidget';

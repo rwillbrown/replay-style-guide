3aff588d91d25bb80f6466cb775ea1b0a4e22e87
Jordan Carmon <jordan@matchstickstudio.co>
2017-04-04 00:13:10 -0500

    Start of cart side bar

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -2,6 +2,7 @@
 
 export * from './site-header/site-header'; // Relative to /src/app/components, this points to the .ts file in each component directory.
 export * from './categories-bar/categories-bar';
+export * from './side-cart/side-cart';
 export * from './buttons/buttons';
 export * from './forms/forms';
 export * from './product-card/product-card';

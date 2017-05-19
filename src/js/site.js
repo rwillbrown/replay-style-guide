0f168e9fee4ad6e01fc36bc1005134d835717464
Jordan Carmon <jordan@matchstickstudio.co>
2017-05-19 10:59:38 -0500

    New category blocks & image sliders on product cards.

--- src/js/site.js
+++ src/js/site.js
@@ -92,4 +92,18 @@ document.addEventListener("DOMContentLoaded", function() {
             });
         }
 
+        /* Swiper for product cards */
+        var mySwiper = new Swiper ('.item-photos', {
+            // Optional parameters
+            direction: 'horizontal',
+            loop: true,
+
+            // If we need pagination
+            pagination: '.swiper-pagination',
+
+            // Navigation arrows
+            nextButton: '.swiper-button-next',
+            prevButton: '.swiper-button-prev'
+        });
+
 });
